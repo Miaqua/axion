ece74d907729606e2f328542819bf36ca2ba92b2
Miaqua <mishaqadmirak@gmail.com>
2026-06-15 21:32:22 +0400

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Profile, Project, Task
 
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ('assigned_to',)
+
 admin.site.register(Profile)
 admin.site.register(Project)
-admin.site.register(Task)
+admin.site.register(Task,TaskAdmin)
